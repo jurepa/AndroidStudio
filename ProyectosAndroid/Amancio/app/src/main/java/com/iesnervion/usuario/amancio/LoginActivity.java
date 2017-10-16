b2babbcca69af487fe7ad24f6c29ba92185aa9f2
PJarana <pablojosejarana21@gmail.com>
2017-10-16 09:16:52 +0200

    Completado el Log in y navegación activity

---
+++ ProyectosAndroid/Amancio/app/src/main/java/com/iesnervion/usuario/amancio/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.iesnervion.usuario.amancio;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by pjarana on 16/10/17.
+ */
+
+public class LoginActivity extends Activity {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+    public void goHome(View v)
+    {
+        finish();
+    }
+}
--- ProyectosAndroid/Amancio/app/src/main/java/com/iesnervion/usuario/amancio/MainActivity.java
+++ ProyectosAndroid/Amancio/app/src/main/java/com/iesnervion/usuario/amancio/MainActivity.java
@@ -1,8 +1,10 @@
 package com.iesnervion.usuario.amancio;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         //Se puede cambiar también en el xml de AndroidManifest dentro de la etiqueta activity android:screenOrientation="landscape"
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToLogin (View v)
+    {
+        Intent i=new Intent(this,LoginActivity.class);
+        startActivity(i);
     }
 }

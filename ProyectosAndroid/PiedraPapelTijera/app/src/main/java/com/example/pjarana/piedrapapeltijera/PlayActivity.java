5b8fa7e4e9e74e49b6ee8a0720de362c04bbe0ac
PJarana <pablojosejarana21@gmail.com>
2017-12-05 11:08:00 +0100

    piedra papel y tijera

--- ProyectosAndroid/PiedraPapelTijera/app/src/main/java/com/example/pjarana/piedrapapeltijera/PlayActivity.java
+++ ProyectosAndroid/PiedraPapelTijera/app/src/main/java/com/example/pjarana/piedrapapeltijera/PlayActivity.java
@@ -1,5 +1,8 @@
 package com.example.pjarana.piedrapapeltijera;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +11,9 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PlayActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imagenContrincante;
     ImageButton piedra;
@@ -17,14 +22,29 @@ public class PlayActivity extends AppCompatActivity {
     Button otra;
     Button salir;
     TextView descripcionJugada;
+    SharedPreferences estadisticas;
+    SharedPreferences.Editor editor;
+    int contadorGanadas;
+    int contadorPerdidas;
+    int contadorEmpates;
+    int resultadoAnterior;
+    int rachaGanadas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        estadisticas=getSharedPreferences("Stats",Context.MODE_PRIVATE);
+        contadorEmpates=estadisticas.getInt("partidas_empatadas",1);
+        contadorGanadas=estadisticas.getInt("partidas_ganadas",1);
+        contadorPerdidas=estadisticas.getInt("partidas_perdidas",1);
+        rachaGanadas=0;
         imagenContrincante=(ImageView)findViewById(R.id.imagenContrincante);
         piedra=(ImageButton)findViewById(R.id.piedra);
         papel=(ImageButton)findViewById(R.id.papel);
         tijeras=(ImageButton)findViewById(R.id.tijeras);
+        piedra.setOnClickListener(this);
+        tijeras.setOnClickListener(this);
+        papel.setOnClickListener(this);
         otra=(Button)findViewById(R.id.jugarOtraVez);
         salir=(Button)findViewById(R.id.salir);
         descripcionJugada=(TextView)findViewById(R.id.descripcionJugada);
@@ -42,7 +62,137 @@ public class PlayActivity extends AppCompatActivity {
                 descripcionJugada.setText("");
                 otra.setVisibility(View.INVISIBLE);
                 salir.setVisibility(View.INVISIBLE);
+                piedra.setVisibility(View.VISIBLE);
+                tijeras.setVisibility(View.VISIBLE);
+                papel.setVisibility(View.VISIBLE);
+                piedra.setClickable(true);
+                tijeras.setClickable(true);
+                papel.setClickable(true);
             }
         });
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        Random rnd=new Random();
+        int decisionPC=rnd.nextInt(3);
+        int resultado;
+        salir.setVisibility(View.VISIBLE);
+        otra.setVisibility(View.VISIBLE);
+        imagenContrincante.setVisibility(View.VISIBLE);
+        switch ( decisionPC)
+        {
+            case 0:
+                imagenContrincante.setImageResource(R.drawable.rsz_tijeras);
+                imagenContrincante.setTag(R.drawable.rsz_tijeras);
+                break;
+            case 1:
+                imagenContrincante.setImageResource(R.drawable.rsz_papel);
+                imagenContrincante.setTag(R.drawable.rsz_papel);
+                break;
+            case 2:
+                imagenContrincante.setImageResource(R.drawable.rsz_piedra);
+                imagenContrincante.setTag(R.drawable.rsz_piedra);
+                break;
+        }
+        switch(v.getId())
+        {
+            case R.id.papel:
+                piedra.setVisibility(View.INVISIBLE);
+                tijeras.setVisibility(View.INVISIBLE);
+                papel.setClickable(false);
+                break;
+            case R.id.tijeras:
+                papel.setVisibility(View.INVISIBLE);
+                piedra.setVisibility(View.INVISIBLE);
+                tijeras.setClickable(false);
+                break;
+            case R.id.piedra:
+                papel.setVisibility(View.INVISIBLE);
+                tijeras.setVisibility(View.INVISIBLE);
+                piedra.setClickable(false);
+                break;
+        }
+        resultado=comprobarResultado(imagenContrincante,v.getId());
+        descripcionJugada.setTextColor(Color.RED);
+        editor=estadisticas.edit();
+        switch (resultado)
+        {
+            case 0:
+                descripcionJugada.setText("EMPATE!"+contadorEmpates);
+                contadorEmpates++;
+                editor.putInt("partidas_empatadas",contadorEmpates);
+                break;
+            case 1:
+                descripcionJugada.setText("GANASTE!"+contadorGanadas);
+                contadorGanadas++;
+                if(rachaGanadas==0)
+                {
+                    rachaGanadas++;
+                }
+                editor.putInt("partidas_ganadas",contadorGanadas);
+                break;
+            case -1:
+                descripcionJugada.setText("PERDISTE..."+contadorPerdidas);
+                contadorPerdidas++;
+                editor.putInt("partidas_perdidas",contadorPerdidas);
+                break;
+        }
+        if(resultadoAnterior==1 && resultado==1 &&rachaGanadas>1)
+        {
+            rachaGanadas++;
+            if(rachaGanadas>estadisticas.getInt("racha_ganadas",0))
+            {
+                editor.putInt("racha_ganadas", rachaGanadas);
+            }
+        }
+        else if(resultadoAnterior!=1 && resultado!=1 &&rachaGanadas>1)
+        {
+            rachaGanadas=0;
+        }
+        resultadoAnterior=resultado;
+        editor.commit();
+    }
+
+    public int comprobarResultado(ImageView contrincante, int imagenUsuario)
+    {
+        int resultado=0;
+        if(imagenUsuario==R.id.piedra)
+        {
+            if((int)contrincante.getTag()==R.drawable.rsz_tijeras)
+            {
+                resultado=1;
+            }
+            else if((int)contrincante.getTag()==R.drawable.rsz_papel)
+            {
+                resultado=-1;
+            }
+
+        }
+        else if (imagenUsuario==R.id.papel)
+        {
+            if((int)contrincante.getTag()==R.drawable.rsz_tijeras)
+            {
+                resultado=-1;
+            }
+            else if((int)contrincante.getTag()==R.drawable.rsz_piedra)
+            {
+                resultado=1;
+            }
+        }
+        else
+        {
+            if((int)contrincante.getTag()==R.drawable.rsz_papel)
+            {
+                resultado=1;
+            }
+            else if((int)contrincante.getTag()==R.drawable.rsz_piedra)
+            {
+                resultado=-1;
+            }
+        }
+        return resultado;
     }
 }
--- ProyectosAndroid/PiedraPapelTijera/app/src/main/java/com/example/pjarana/piedrapapeltijera/StatsActivity.java
+++ ProyectosAndroid/PiedraPapelTijera/app/src/main/java/com/example/pjarana/piedrapapeltijera/StatsActivity.java
@@ -1,13 +1,30 @@
 package com.example.pjarana.piedrapapeltijera;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StatsActivity extends AppCompatActivity {
 
+    SharedPreferences estadisticas;
+    TextView partidasGanadas;
+    TextView partidasEmpatadas;
+    TextView partidasPerdidas;
+    TextView rachaGanadas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        partidasGanadas=(TextView)findViewById(R.id.partidasGanadas);
+        partidasEmpatadas=(TextView)findViewById(R.id.partidasEmpatadas);
+        partidasPerdidas=(TextView)findViewById(R.id.partidasPerdidas);
+        rachaGanadas=(TextView)findViewById(R.id.rachaGanadas);
+        estadisticas=getSharedPreferences("Stats",Context.MODE_PRIVATE);
+        partidasGanadas.setText(partidasGanadas.getText()+" "+estadisticas.getInt("partidas_ganadas",0));
+        partidasEmpatadas.setText(partidasEmpatadas.getText()+" "+estadisticas.getInt("partidas_empatadas",0));
+        partidasPerdidas.setText(partidasPerdidas.getText()+" "+estadisticas.getInt("partidas_perdidas",0));
+        rachaGanadas.setText(rachaGanadas.getText()+" "+estadisticas.getInt("racha_ganadas",0));
     }
 }

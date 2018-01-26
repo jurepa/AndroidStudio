88c409fbe75e3196626c6e4a2f1153f2d7c3f81f
PJarana <pablojosejarana21@gmail.com>
2018-01-26 10:14:22 +0100

    Auto stash before merge of "master" and "origin/master"

--- ProyectosAndroid/NeverStopClicking/app/src/main/java/com/iesnervion/pjarana/neverstopclicking/MainActivity.java
+++ ProyectosAndroid/NeverStopClicking/app/src/main/java/com/iesnervion/pjarana/neverstopclicking/MainActivity.java
@@ -1,6 +1,9 @@
 package com.iesnervion.pjarana.neverstopclicking;
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button btnSalir;
+    Button btnJugar;
     final int REQUEST_ENABLE_BT=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         MediaPlayer musicTetris=MediaPlayer.create(this,R.raw.tetris);
         musicTetris.start();
         btnSalir=findViewById(R.id.btnSalir);
-        btnSalir.setOnClickListener(new View.OnClickListener() {
+        btnJugar=findViewById(R.id.btnJugar);
+        btnJugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -34,12 +39,37 @@ public class MainActivity extends AppCompatActivity {
                         Intent activarBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                         startActivityForResult(activarBluetooth, REQUEST_ENABLE_BT);
                     }
+<<<<<<< Updated upstream
                     /*else
                     {
                         bluetoothAdapter.disable();
                         Toast.makeText(getApplicationContext(),"Se ha desactivado el bluetooth",Toast.LENGTH_LONG).show();
                     }*/
+=======
+>>>>>>> Stashed changes
                 }
+            }
+        });
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("¿Seguro que quieres salir?");
+                builder.setTitle("Salir");
+                builder.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                builder.setNegativeButton("Seguir jugando", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
             }
         });
 
